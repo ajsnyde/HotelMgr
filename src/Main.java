26ac327c41d902c735fe94c0c90c86173bd13996
ajsnyde <ajsnyde@bgsu.edu>
2016-02-12 17:48:45 -0500

    First Commit
    Pretty much settled for an open source 3rd-party calendar class, added
    some dialog windows and functionality, and finally changed the look and
    feel to Windows (standard java looks like cheap garbage).

---
+++ src/Main.java
@@ -0,0 +1,114 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+
+public class Main {
+
+	private JFrame frmMainWindow;
+	private JTextField startDateField;
+	private JTextField endDateField;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+
+				try {
+					UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (InstantiationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IllegalAccessException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				try {
+					Main window = new Main();
+					window.frmMainWindow.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmMainWindow = new JFrame();
+		frmMainWindow.setTitle("Main Window");
+		frmMainWindow.setBounds(100, 100, 450, 300);
+		frmMainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmMainWindow.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		JPanel startPanel = new JPanel();
+		frmMainWindow.getContentPane().add(startPanel);
+
+		JLabel lblStartDate = new JLabel("Start Date:");
+		startPanel.add(lblStartDate);
+		startDateField = new JTextField();
+		startDateField.setEnabled(false);
+		startDateField.setColumns(20);
+		startPanel.add(startDateField);
+		JButton btnSelectStartDate = new JButton("Select Date!");
+		startPanel.add(btnSelectStartDate);
+		btnSelectStartDate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				final SelectDate startDateDialog = new SelectDate();
+				startDateDialog.addWindowStateListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						startDateField.setText(startDateDialog.datePanel.getDate().toString());
+						startDateDialog.killSelf();
+					}
+				});
+			}
+		});
+
+		JPanel endPanel = new JPanel();
+		frmMainWindow.getContentPane().add(endPanel);
+
+		JLabel lblEndDate = new JLabel("End Date:");
+		endPanel.add(lblEndDate);
+
+		endDateField = new JTextField();
+		endDateField.setEnabled(false);
+		endDateField.setColumns(20);
+		endPanel.add(endDateField);
+
+		JButton btnSelectEndDate = new JButton("Select Date!");
+		endPanel.add(btnSelectEndDate);
+		btnSelectEndDate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				final SelectDate endDateDialog = new SelectDate();
+				endDateDialog.addWindowStateListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						endDateField.setText(endDateDialog.datePanel.getDate().toString());
+						endDateDialog.killSelf();
+					}
+				});
+			}
+		});
+	}
+}
---
+++ src/SelectDate.java
@@ -0,0 +1,72 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Window.Type;
+
+public class SelectDate extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	JButton okButton = new JButton("OK");
+	final DateChooserPanel datePanel = new DateChooserPanel();
+	/**
+	 * Launch the application.
+	 */
+	public static void main() {
+		try {
+			SelectDate dialog = new SelectDate();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public SelectDate() {
+		setType(Type.POPUP);
+		setTitle("Date Selection");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(datePanel, BorderLayout.CENTER);
+		getContentPane().add(contentPanel, BorderLayout.SOUTH);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						killSelf();
+					}
+				});
+				buttonPane.add(cancelButton);
+			}
+		}
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	void killSelf(){
+		setVisible(false);
+		dispose();
+	}
+
+	public void addWindowStateListener(ActionListener actionListener) {
+		okButton.addActionListener(actionListener);
+	}
+}
+
